23f2d91036be3da262362383479b4b6cfecc340b
Nazrulofficeial <hmraz101@gmail.com>
2024-02-19 19:49:37 +0600

    Seat Color Changing and availability decrease finished

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const changeMyColoors = document.querySelectorAll(".color-changer");
+
+    changeMyColoors.forEach(function(changeMyColor) {
+        changeMyColor.addEventListener("click", function changerBtnColors() {
+            changeMyColor.style.backgroundColor = "#1DD100";
+            changeMyColor.style.color = "#ffffff";
+            changeMyColor.removeEventListener("click", changerBtnColors);
+        });
+      });
+    
+    // Availability Decrease by 1 if I press in a btn
+    // And Disable the btn when I click on 4 bton
+    const availabilityCountElement = document.getElementById("availability-count");
+    let availabilityCount = 16;
+    let clickedButtonCount = 0;
+  
+    availabilityCountElement.textContent = availabilityCount;
+  
+    const buttons = document.querySelectorAll(".color-changer");
+    buttons.forEach(function(button) {
+      button.addEventListener("click", function() {
+        clickedButtonCount++;
+  
+        availabilityCount--;
+  
+        availabilityCountElement.textContent = availabilityCount;
+  
+        if (clickedButtonCount === 4) {
+          buttons.forEach(function(btn) {
+            if (!btn.disabled) {
+              btn.disabled = true;
+            }
+          });
+        }
+  
+        // If I clicked on 4 buttons the rest of buttons are disabled
+        button.disabled = true;
+      });
+    });
+  });
